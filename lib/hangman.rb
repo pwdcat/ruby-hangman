f9607f9e8c23b371eda2817fd293712381a2d365
pwdcat <113306139+pwdcat@users.noreply.github.com>
2024-01-11 18:42:59 -0500

    Added Lives, Guessing System Able to guess and shows what you've gotten right

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -15,4 +15,27 @@ word = dictionary.keys.sample
 
 guessArray = Array.new(dictionary[word], '_')
 
-p word
+def getInput
+  loop do
+    puts 'Enter a letter: '
+    input = gets.chomp
+    return input if input.length == 1
+
+    puts 'Invalid Input'
+  end
+end
+
+lives = 6
+
+while lives.positive?
+  p lives
+  p guessArray.join(' ')
+  answer = getInput
+  if word.include?(answer)
+    guessArray.map!.with_index do |_text, index|
+      word[index] == answer ? word[index] : guessArray[index]
+    end
+  else
+    lives -= 1
+  end
+end
